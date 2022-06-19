d034bf9f6656ff2ddc716ab66691d26c09e77f3b
Gabrielle Befort <mrsbefort@gmail.com>
2022-06-19 16:01:19 -0400

    adding post and write save routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
  //Import express from express
- const express = require('express');
+ const e = require('express');
+const express = require('express');
  const fs = require("fs");
  const util = require("util");
  const readFile = util.promisify(fs.readFile);
@@ -8,20 +9,64 @@
 
  //App is storing the express server
  const app = express();
+//middle ware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
  //Setting up different get routes
 app.get('/notes', (req, res) => {
     res.sendFile(__dirname + '/public/notes.html');
 });
 //reads contents in db folder
 app.get('/api/notes',async (req, res) => {
-    const readAPIFile = await readFile(__dirname + "/db/db.json");
-    res.json(readAPIFile);
+    const readAPIFile = await readFile("./db/db.json");
+    console.log(readAPIFile);
+    const parsedreadFile = await JSON.parse(readAPIFile);
+    res.json(parsedreadFile);
 });
 
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
+
+app.post('/api/notes', (req, res) => {
+const readAPIFile = req.body;
+// review_id: uuid()
+console.log(req.body);
+fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedReviews = JSON.parse(data);
+
+        // user created a new note and sent it in body
+        parsedReviews.push(req.body);
+        //writing updated new notes to file
+        // Write updated reviews back to the file
+        fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedReviews),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+          );
+        }
+
+})
+//adding a response
+const response = {
+    status: 'success',
+    body: req.body,
+  };
+
+  console.log(response);
+  res.json(response);
+
+});
+
 
 app.listen(PORT, () =>
 console.log(`listening on port http://localhost:${PORT}`)
